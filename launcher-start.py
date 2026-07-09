6963dc7a55dcf7d5376c36155d70d5c2e2f2b83f
Dani <daniell0626@naver.com>
2026-07-09 06:04:33 +0000

    app.py 위치 번경 & launcher start 수정

--- launcher-start.py
+++ launcher-start.py
@@ -1,10 +1,8 @@
-#백엔드 실행 그리고 gui 실행하는 python 코드.
-#USer가 이 파일 실행하면 자동으로 백앤드 둥 다 열림
+#백엔드 실행하는 python 코드.
+#USer가 이 파일 실행하면 자동으로 백앤드 열림
 import subprocess
 import sys
 #백엔드 실행
-subprocess.Popen(['python', 'backend.py'])
-#GUI 실행
-subprocess.Popen(['python', 'gui.py'])
-print("백엔드와 GUI가 실행되었습니다.")
+subprocess.Popen(['python', './backend/app.py'])
+print("백엔드가 실행되었습니다.")
 sys.exit()
