d39b3334a80a90fcab091f08a544388bf2d98e0c
Dani <daniell0626@naver.com>
2026-07-10 07:18:37 +0000

    지구 실시간 제작

--- frontend/templates/os-luncher/script.js
+++ frontend/templates/os-luncher/script.js
@@ -16,6 +16,7 @@
             '설정': './apps/settings.html',
             '파일 암호화': './apps/filesecurety.html',
             'game': "./apps/game.html",
+            '실시간 지구 동영상': './apps/earthlive.html',
             '업데이트 로그': "./apps/updateLog.html"
         };
 
